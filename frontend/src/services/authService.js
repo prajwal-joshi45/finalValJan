0a5865680f7ed6104bdf7a8a30d567e7cab88919
prajwal-joshi45 <91948285+prajwal-joshi45@users.noreply.github.com>
2025-01-31 09:38:39 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://finalvaljan-backend.onrender.com';
 
 export const register = async (userData) => {
   console.log('Sending registration data:', userData); // Add this log
